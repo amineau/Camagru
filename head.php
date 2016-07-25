9c2e1ac224b89a1fbb87177a91ca83677182b2e9
mineau <antoine.mineau@hotmail.fr>
2016-07-25 21:37:05 +0000

    webcam activée

--- head.php
+++
@@ -1,8 +0,0 @@
-	<head>
-		<title>Camagru</title>
-		<meta charset="utf-8" />
-		<link rel="stylesheet" type="text/css" href="index.css">
-		<script type="text/javascript" src="script.js"></script>
-	</head>
-	
-	<?php $website_name = "Instagru"; ?>
